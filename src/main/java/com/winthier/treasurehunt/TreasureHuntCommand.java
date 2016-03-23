6b0ee4fe6fababc9dd5aae70c972baf787b30893
Thomas Loy <ths.loy@googlemail.com>
2016-03-23 16:10:01 +0100

    Display how many players found a treasure already

--- src/main/java/com/winthier/treasurehunt/TreasureHuntCommand.java
+++ src/main/java/com/winthier/treasurehunt/TreasureHuntCommand.java
@@ -21,9 +21,9 @@ public class TreasureHuntCommand implements CommandExecutor {
         plugin.msg(player, "&a&lTreasure Hunt&r List");
         for (Treasure treasure: plugin.getTreasureConfig().getTreasures()) {
             if (treasure.hasFound(player)) {
-                plugin.msg(player, " [&ax&r]&a %s", treasure.getName());
+                plugin.msg(player, " [&ax&r]&a %s &8(found by %d)", treasure.getName(), treasure.getFound().size());
             } else {
-                plugin.msg(player, " [&8?&r]&r %s", treasure.getName());
+                plugin.msg(player, " [&8?&r]&r %s &8(found by %d)", treasure.getName(), treasure.getFound().size());
             }
         }
         return true;
